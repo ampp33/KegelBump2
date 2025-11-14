3473409f4372223fedc6f10ef66bd40703ae8ec2
Benjamin Sigby <ampp33@gmail.com>
2025-11-14 11:04:50 -0600

    Initial Commit

---
+++ KegelBump2 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KegelBump2 Watch App
+//
+//  Created by Kevin Kunst on 11/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KegelBump2 Watch App/KegelBump2App.swift
@@ -0,0 +1,17 @@
+//
+//  KegelBump2App.swift
+//  KegelBump2 Watch App
+//
+//  Created by Kevin Kunst on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct KegelBump2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
